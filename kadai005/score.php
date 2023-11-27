94c2d3ff8defcd7c132c07299657a9206cd5b1b1
onogod <144659993+onogod@users.noreply.github.com>
2023-11-27 12:10:28 +0900

    Add files via upload

---
+++ kadai005/score.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a>
+        <?php
+         $score1 = 80;
+    
+         $score2 = 60;
+        
+        $score3 = 55;
+      
+        $score4 = 40;
+     
+        $score5 = 100;
+     
+        $score6 = 25;
+   
+        $score7 = 80;
+       
+         $score8 = 95;
+     
+         $score9 = 30;
+      
+         $score10 = 60;
+    
+       
+         $total_score = $score1 + $score2 + $score3 + $score4 + $score5 +  $score6 + $score7 + $score8 + $score9 + $score10;
+         
+       echo $total_score;
+       echo'<br>';
+         $ave_rage = $total_score / 10;
+         echo'<br>';
+         echo $ave_rage; 
+
+      
+        ?>
+    </a>
+    
+</body>
+</html>
